12109973f58e71131a083a4518291a6a58ee8c6d
Alex Band <lingualex12345@gmail.com>
2024-01-29 15:47:34 -0600

    Finish the project

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let grid = 16;
 let subGrid = Math.round((230 * grid) / 100);
 const setUpButton = document.querySelector('.set-grid');
 let randomColor = false;
+let gradualColoring = false;
 setUpButton.addEventListener('click', () => {
   grid = Number(prompt('How many rows would you like to have? The number should not exceed 100', grid));
   subGrid = Math.round((230 * grid) / 100);
@@ -26,6 +27,17 @@ randomColorButton.addEventListener('click', () => {
   }
 });
 
+const gradualColorButton = document.querySelector('.gradual-coloring');
+gradualColorButton.addEventListener('click', () => {
+  if (gradualColoring === false) {
+    gradualColoring = true;
+    gradualColorButton.classList.add('is-gradual');
+  } else if (gradualColoring === true) {
+    gradualColoring = false;
+    gradualColorButton.classList.remove('is-gradual');
+  }
+});
+
 const changeGrid = (grid, subGrid) => {
   holder.innerHTML = '';
 
@@ -33,7 +45,7 @@ const changeGrid = (grid, subGrid) => {
     const div = document.createElement('div');
     div.className = `${i}`;
     div.style.display = 'flex';
-    div.style.backgroundColor = 'black';
+    div.style.backgroundColor = 'white';
     div.style.justifyContent = 'center';
     div.style.flex = 1;
     
@@ -44,16 +56,24 @@ const changeGrid = (grid, subGrid) => {
       subDiv.style.backgroundColor = 'white';
       subDiv.style.flex = 1;
       subDiv.addEventListener('mouseover', (e) => {
-        if (randomColor === true && e.buttons == 1) {
+        if ((randomColor === true && e.buttons == 1) && (gradualColoring === true && e.buttons == 1)) {
+          gradualRandomColorIn(e.target);
+        } else if (randomColor === true && e.buttons == 1) {
           randomColorIn(e.target);
+        } else if (gradualColoring === true && e.buttons == 1) {
+          gradualColorIn(e.target);
         } else if (e.buttons == 1 && randomColor == false) {
           colorIn(e.target);
         }
       });
 
       subDiv.addEventListener('click', (e) => {
-        if (randomColor === true && e.buttons == 0) {
+        if ((randomColor === true && e.buttons == 0) && (gradualColoring === true && e.buttons == 0)) {
+          gradualRandomColorIn(e.target);
+        } else if (randomColor === true && e.buttons == 0) {
           randomColorIn(e.target);
+        } else if (gradualColoring === true && e.buttons == 0) {
+          gradualColorIn(e.target)
         } else if (e.buttons == 0 && randomColor === false) {
           colorIn(e.target);
         }
@@ -63,8 +83,62 @@ const changeGrid = (grid, subGrid) => {
   }
 }
 
+
+const gradualColorIn = (target) => {
+  let currentColor = target.style.backgroundColor;
+  console.log(currentColor)
+  let currentAlpha = currentColor;
+  if (currentAlpha.split(',').length === 4) {
+    console.log(currentAlpha);
+    let targetAlpha = currentColor;
+    let rgbaArr = [];
+    for (let i = 0; i < targetAlpha.length; i++) {
+      if (targetAlpha[i].match(/\d/g)) {
+        rgbaArr.push(targetAlpha[i]);
+      }
+    }
+    rgbaArr = rgbaArr.slice(3);
+    targetAlpha = `${rgbaArr.at(-2)}.${rgbaArr.at(-1)}`;
+    targetAlpha = Number(targetAlpha);
+    target.style.backgroundColor = `rgba(0, 0, 0, ${targetAlpha + 0.1})`;
+  }
+
+  if (currentColor === 'white') {
+    target.style.backgroundColor = `rgba(0, 0, 0, 0.1)`;
+  }
+}
+
 const colorIn = (target) => {
   target.style.backgroundColor = 'black';
+}
+
+const gradualRandomColorIn = (target) => {
+  let currentColor = target.style.backgroundColor;
+  let currentAlpha = currentColor;
+  if (currentAlpha.split(',').length === 4) {
+    let targetAlpha = currentColor;
+    let rgbaArr = [];
+    for (let i = 0; i < targetAlpha.length; i++) {
+      if (targetAlpha[i].match(/\d/g)) {
+        rgbaArr.push(targetAlpha[i]);
+      }
+    }
+    targetAlpha = `${rgbaArr.at(-2)}.${rgbaArr.at(-1)}`;
+    targetAlpha = Number(targetAlpha);
+    const randomColor1 = (Math.floor(Math.random() * (255 - 1 + 1)) + 1);
+    const randomColor2 = (Math.floor(Math.random() * (255 - 1 + 1)) + 1);
+    const randomColor3 = (Math.floor(Math.random() * (255 - 1 + 1)) + 1);
+    target.style.backgroundColor = `rgba(${randomColor1}, ${randomColor2}, ${randomColor3}, ${Number((targetAlpha + 0.1).toFixed(1))})`;
+    return;
+  }
+
+
+  if (currentColor === 'white') {
+    const randomColor1 = (Math.floor(Math.random() * (255 - 1 + 1)) + 1);
+    const randomColor2 = (Math.floor(Math.random() * (255 - 1 + 1)) + 1);
+    const randomColor3 = (Math.floor(Math.random() * (255 - 1 + 1)) + 1);
+    target.style.backgroundColor = `rgba(${randomColor1}, ${randomColor2}, ${randomColor3}, 0.1)`;
+  }
 }
 
 const randomColorIn = (target) => {
